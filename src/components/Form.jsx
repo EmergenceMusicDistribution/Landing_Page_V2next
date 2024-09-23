0fbf5dccde6c6505e45511a60affd3ac03e091ee
Baudwin <baudwin12@gmail.com>
2024-09-23 17:41:18 +0100

    fixed input box

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,9 +15,11 @@ export const ReusableForm = ({ onSubmit, schema, fields }) => {
         <div className='' key={field.name}>
           <label className='text-gray-400'>{field.label}</label>
           <div className='space-y-1 '>
-            <div className={`flex items-center border rounded-md ${errors[field.name] && 'border border-red-500'} pl-2 pr-4`}>
-          <input placeholder={`Enter ${field.label}`} className={`bg-[#1e1f26] text-sm w-full appearance-none py-3.5 focus:outline-none  text-white `}{...register(field.name)} type={field.type} />
-          {errors[field.name] && <AiOutlineWarning size={23} className='text-red-500'/>}
+            <div className={`${errors[field.name] && ' border-red-500'}`}>
+          <input placeholder={`Enter ${field.label}`} className={`bg-[#1e1f26] rounded-md text-[0.87
+            rem] w-full appearance-none py-3.5 border px-2 focus:outline-none text-white ${errors[field.name] && ' border-red-500'} `}{...register(field.name)} type={field.type} />
+          {/* {errors[field.name] && <AiOutlineWarning size={23} className='text-red-500 mr-2'/>} */}
+       
             </div>
           {errors[field.name] && <p className='text-red-500 text-sm'>{errors[field.name]?.message}</p>}
           </div>
